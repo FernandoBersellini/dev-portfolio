cf195f602dc3be85110e7bc42d662c8e8946dc84
FernandoBersellini <fernandobersellini@outlook.com.br>
2026-03-16 13:58:05 -0300

    feat: add Header component with navigation

---
+++ components/header.tsx
@@ -0,0 +1,31 @@
+import { Globe, Sun } from "lucide-react";
+
+export default function Header() {
+    return (
+        <header className="m-auto mt-3 w-2/3 flex justify-between items-center">
+            <div>
+                <h1>Fernando Bersellini</h1>
+            </div>
+
+            <div className="flex gap-3 items-center">
+                <nav>
+                    <ul className="flex gap-3">
+                        <li><a href="#">Experiências</a></li>
+                        <li><a href="#">Projetos</a></li>
+                        <li><a href="#">Tecnologias</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                </nav>
+
+                <button className="bg-blue-500 text-white px-4 py-2 rounded">
+                    <Globe />
+                </button>
+
+                <button className="bg-blue-500 text-white px-4 py-2 rounded">
+                    <Sun />
+                </button>
+            </div>
+
+        </header>
+    );
+}
